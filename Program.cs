c0e281b3ad2464ab43ac2847b59e40ec104f2354
deepukosuri411 <95608587+deepukosuri411@users.noreply.github.com>
2021-12-06 12:53:44 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Steeltoe.Extensions.Configuration.ConfigServer;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using Unity.Microsoft.DependencyInjection;
+
+namespace OrderStatusService.Api
+{
+    [ExcludeFromCodeCoverage]
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            AppContext.SetSwitch("System.Net.Http.UseSocketsHttpHandler", false);
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateWebHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseConfiguration(new ConfigurationBuilder().AddCommandLine(args).Build());
+                    webBuilder.UseContentRoot(Directory.GetCurrentDirectory());
+                    webBuilder.AddConfigServer();
+                    webBuilder.UseUnityServiceProvider();
+                    webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureServices(services =>
+                {
+                    //services.AddHostedService<RabbitMQService>();
+                });
+    }
+}
---
+++ Startup.ConfigureServices.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using OrderStatusService.Api.MiddleWare.Configurations;
+using OrderStatusService.Api.MiddleWare.Formatter;
+
+namespace OrderStatusService.Api
+{
+    public partial class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            AddCustom(services);
+            AddMvc(services);
+        }
+
+        public void AddMvc(IServiceCollection services)
+        {
+            services.AddMvc(options =>
+            {
+                options.OutputFormatters.Insert(0, new OutputFormatter(Configuration));
+            })
+            .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            //.AddFluentValidation(fvc => fvc.RegisterValidatorsFromAssemblyContaining<Startup>());
+        }
+
+        private void AddCustom(IServiceCollection services)
+        {
+            services.AddCustomServices(Configuration);
+        }
+    }
+}
